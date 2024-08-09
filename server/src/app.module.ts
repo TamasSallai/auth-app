b8ea8c4ab1a08ec64a195a33ee08528a98e20e76
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-08-09 15:16:13 +0200

    nestjs server init

---
+++ server/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { PrismaModule } from './prisma/prisma.module'
+
+@Module({
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, PrismaModule],
+})
+export class AppModule {}
