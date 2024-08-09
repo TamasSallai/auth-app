629b4811c9708485c583b08b697107757a483ed0
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-08-09 14:05:13 +0200

    removed fastify backend

--- server/src/types/types.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-  id: string
-  email: string
-  displayName: string
-  firstName: string | null
-  lastName: string | null
-  isVerified: boolean
-}
--- server/src/utils/customErrors.ts
+++
@@ -1,7 +0,0 @@
-import createError from '@fastify/error'
-
-export const NotFoundError = createError('NOT_FOUND', '%s', 404)
-
-export const UnauthorizedError = createError('UNAUTHORIZED', '%s', 401)
-
-export const ConflictError = createError('CONFLICT', '%s', 409)
